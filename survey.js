a090047b1ee487d43c9228f24476f4d3fa6ee51a
Kershia1 <insatiable.meerkat@gmail.com>
2023-08-08 05:31:38 +0000

    survey.js

--- survey.js
+++ survey.js
@@ -23,7 +23,7 @@ What is your superpower? In a few words, tell us what you are amazing at!
 //modualrize and refactor the code
 // give each question a function, so each only handles 1 item
 // use await to wait for the answer to be inputted before next prompt
-// prompt also needs to be used to start the survey..how?
+// prompt also needs to be used to start the survey..how? Looks like async await and promises are needed.... dum dum dum 
 
 const readline = require('readline');
 
@@ -35,78 +35,82 @@ class Survey {
     });
   }
 
-  queryName() {
+
+  queryName(name) {
     this.r1.question('What is your name? You can use a nickname as well. ', (answer) => {
-      //this is a prompt awaiting input from the user 
-      console.log(`Thank you for your valuable feedback: ${answer}`);
-      this.r1.close();
+      name(answer);
     });
   }
 
-  queryActivity() {
+  queryActivity(activity) {
     this.r1.question('What is an activity you like doing?', (answer) => {
-      //this is a prompt awaiting input from the user 
-      console.log(`Thank you for your valuable feedback: ${answer}`);
-
-      this.r1.close();
+      activity(answer);
     });
   }
 
-  queryMusic() {
+  queryMusic(music) {
     this.r1.question('What do you listen to while doing that?', (answer) => {
-      //this is a prompt awaiting input from the user 
-      console.log(`Thank you for your valuable feedback: ${answer}`);
-
-      this.r1.close();
+      music(answer);
     });
   }
 
-  queryMeal() {
+  queryMeal(meal) {
     this.r1.question('Which meal is your favourite (eg: dinner, brunch, etc.)', (answer) => {
-      //this is a prompt awaiting input from the user 
-      console.log(`Thank you for your valuable feedback: ${answer}`);
-
-      this.r1.close();
+      meal(answer);
     });
   }
 
-  queryFood() {
+  queryFood(food) {
     this.r1.question('What\'s your favourite thing to eat for that meal?', (answer) => {
-      //this is a prompt awaiting input from the user 
-      console.log(`Thank you for your valuable feedback: ${answer}`);
-
-      this.r1.close();
+      food(answer);
     });
   }
 
-  querySport() {
+  querySport(sport) {
     this.r1.question('Which sport is your absolute favourite?', (answer) => {
-      //this is a prompt awaiting input from the user 
-      console.log(`Thank you for your valuable feedback: ${answer}`);
-
-      this.r1.close();
+      sport(answer);
     });
   }
 
-  querySuperpower() {
+  querySuperpower(superpower) {
     this.r1.question('What is your superpower? In a few words, tell us what you are amazing at!', (answer) => {
-      //this is a prompt awaiting input from the user 
-      console.log(`Thank you for your valuable feedback: ${answer}`);
+      superpower(answer);
+    });
+  }
 
-      this.r1.close();
+  begin() { //this is the function that starts the survey
+    //each function has a separate callback
+    this.queryName((name) => {
+      this.queryActivity((activity) => {
+        this.queryMusic((music) => {
+          this.queryMeal((meal) => {
+            this.queryFood((food) => {
+              this.querySport((sport) => {
+                this.querySuperpower((superpower) => {
+                  console.log(`Thank you for your valuable feedback: ${answer}`);
+                  this.r1.close(); // stops the survey
+                });
+              });
+            });
+          });
+        });
+      });
     });
   }
 }
 
 //Driver Code
 const survey = new Survey();
-survey.queryName();
-survey.queryActivity();
-survey.queryMusic();
-survey.queryMeal();
-survey.queryFood();
-survey.querySport();
-survey.querySuperpower();
+survey.begin();
+
+
+// survey.queryName();
+// survey.queryActivity();
+// survey.queryMusic();
+// survey.queryMeal();
+// survey.queryFood();
+// survey.querySport();
+// survey.querySuperpower();
 
 
 
