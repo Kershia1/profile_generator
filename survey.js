c040493ec172ffa7262c46e37d98d51c11824de1
Kershia1 <insatiable.meerkat@gmail.com>
2023-08-08 05:03:38 +0000

    survey.js

--- survey.js
+++ survey.js
@@ -21,85 +21,94 @@ Which sport is your absolute favourite?
 What is your superpower? In a few words, tell us what you are amazing at!
 */
 //modualrize and refactor the code
+// give each question a function, so each only handles 1 item
+// use await to wait for the answer to be inputted before next prompt
+// prompt also needs to be used to start the survey..how?
+
+const readline = require('readline');
+
 class Survey {
-  constructor() { 
-    this.r1 =readline.createInterface({
+  constructor() {
+    this.r1 = readline.createInterface({
       input: process.stdin,
       output: process.stdout
     });
-  //   this.name = '';
-  //   this.activity = '';
-  //   this.music = '';
-  //   this.meal = '';
-  //   this.food = '';
-  //   this.sport = '';
-  //   this.superpower = '';
-  // }
-}
-queryName() {
-  this.r1.question('What is your name? You can use a nickname as well. ', (answer) => {
-    //this is a prompt awaiting input from the user 
-    console.log(`Thank you for your valuable feedback: ${answer}`);
-    this.r1.close();
-  });
-}
+  }
 
-queryActivity() {
-  this.r1.question('What is an activity you like doing?', (answer) => {
-    //this is a prompt awaiting input from the user 
-    console.log(`Thank you for your valuable feedback: ${answer}`);
+  queryName() {
+    this.r1.question('What is your name? You can use a nickname as well. ', (answer) => {
+      //this is a prompt awaiting input from the user 
+      console.log(`Thank you for your valuable feedback: ${answer}`);
+      this.r1.close();
+    });
+  }
 
-    this.r1.close();
-  });
-}
+  queryActivity() {
+    this.r1.question('What is an activity you like doing?', (answer) => {
+      //this is a prompt awaiting input from the user 
+      console.log(`Thank you for your valuable feedback: ${answer}`);
 
-queryMusic() {
-  this.r1.question('What do you listen to while doing that?', (answer) => {
-    //this is a prompt awaiting input from the user 
-    console.log(`Thank you for your valuable feedback: ${answer}`);
+      this.r1.close();
+    });
+  }
 
-    this.r1.close();
-  });
-}
+  queryMusic() {
+    this.r1.question('What do you listen to while doing that?', (answer) => {
+      //this is a prompt awaiting input from the user 
+      console.log(`Thank you for your valuable feedback: ${answer}`);
 
-queryMeal() {
-  this.r1.question('Which meal is your favourite (eg: dinner, brunch, etc.)', (answer) => {
-    //this is a prompt awaiting input from the user 
-    console.log(`Thank you for your valuable feedback: ${answer}`);
+      this.r1.close();
+    });
+  }
 
-    this.r1.close();
-  });
-}
+  queryMeal() {
+    this.r1.question('Which meal is your favourite (eg: dinner, brunch, etc.)', (answer) => {
+      //this is a prompt awaiting input from the user 
+      console.log(`Thank you for your valuable feedback: ${answer}`);
 
-queryFood() {
-  this.r1.question('What\'s your favourite thing to eat for that meal?', (answer) => {
-    //this is a prompt awaiting input from the user 
-    console.log(`Thank you for your valuable feedback: ${answer}`);
+      this.r1.close();
+    });
+  }
 
-    this.r1.close();
-  });
-}
+  queryFood() {
+    this.r1.question('What\'s your favourite thing to eat for that meal?', (answer) => {
+      //this is a prompt awaiting input from the user 
+      console.log(`Thank you for your valuable feedback: ${answer}`);
 
-querySport() {
-  this.r1.question('Which sport is your absolute favourite?', (answer) => {
-    //this is a prompt awaiting input from the user 
-    console.log(`Thank you for your valuable feedback: ${answer}`);
+      this.r1.close();
+    });
+  }
 
-    this.r1.close();
-  });
-}
+  querySport() {
+    this.r1.question('Which sport is your absolute favourite?', (answer) => {
+      //this is a prompt awaiting input from the user 
+      console.log(`Thank you for your valuable feedback: ${answer}`);
 
-querySuperpower() {
-  this.r1.question('What is your superpower? In a few words, tell us what you are amazing at!', (answer) => {
-    //this is a prompt awaiting input from the user 
-    console.log(`Thank you for your valuable feedback: ${answer}`);
+      this.r1.close();
+    });
+  }
 
-    this.r1.close();
-  });
-}
+  querySuperpower() {
+    this.r1.question('What is your superpower? In a few words, tell us what you are amazing at!', (answer) => {
+      //this is a prompt awaiting input from the user 
+      console.log(`Thank you for your valuable feedback: ${answer}`);
+
+      this.r1.close();
+    });
+  }
 }
 
-// const readline = require('readline');
+//Driver Code
+const survey = new Survey();
+survey.queryName();
+survey.queryActivity();
+survey.queryMusic();
+survey.queryMeal();
+survey.queryFood();
+survey.querySport();
+survey.querySuperpower();
+
+
 
 // const rl = readline.createInterface({
 //   input: process.stdin,
